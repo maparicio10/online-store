62dc6afe4d9004c021219da14711f2417a98aa79
maikel aparicio <maparicio10@gmail.com>
2024-08-07 00:08:59 -0400

    Consume fake api

--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -1,12 +1,25 @@
+import { useEffect, useState } from "react"
 import Card from "../../Components/Card"
 import Layout from "../../Components/Layout"
 
 function Home() {
+  const [products, setProducts] = useState([])
+
+  useEffect(() => {
+    fetch('https://api.escuelajs.co/api/v1/products')
+      .then(response => response.json())
+      .then(data => setProducts(data))
+  }, [])
 
   return (
     <Layout>
-       Home
-       <Card />
+      Home
+      <div className="grid gap-4 grid-cols-4 w-full max-w-screen-lg">
+        {products?.map(product => (
+          <Card key={product.key} data={product} />
+        ))
+        }
+      </div>
     </Layout>
   )
 }
